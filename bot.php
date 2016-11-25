7da540baadcd61b3963ee67d2724307be4a518ae
MacTanomsup <nathapol_t@kkumail.com>
2016-11-26 02:06:05 +0700

    edit bot.php

--- bot.php
+++ bot.php
@@ -16,10 +16,12 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
+			$userId = $events['sources']['userId'];
+
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text.":".$userId
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
